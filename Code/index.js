8d03b15ecffe2396d07929c6efc8a1581e9c29cf
VuThiDiu <56347983+VuThiDiu@users.noreply.github.com>
2020-05-16 23:21:36 +0700

    Post Data

--- Code/index.js
+++ Code/index.js
@@ -1,12 +1,19 @@
 const express= require('express')
 const app = express();
 const port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 var  users=[
     {id :1, name:'Vũ Trang Linh'},
     {id :2, name:'Nguyễn Lê Bảo Thanh'},
     {id :3, name:'Phạm Bạch Tuyết'},
     {id :4, name:'Nguyễn Linh Nam'},
 ]
+
+
 app.set('view engine','pug');
 app.set('views', './views');
 
@@ -18,6 +25,9 @@ app.get('/users', function(req, res){
         users: users
     })
 })
+
+// function search
+
 app.get('/users/search', function (req, res){
     var  q=  req.query.q;
     var matchedUsers = users.filter(function(user){
@@ -27,6 +37,25 @@ app.get('/users/search', function (req, res){
         users: matchedUsers
     })
 });
+
+// function create  
+app.get('/users/create', function(req, res){
+    var  q=  req.query.q;
+var matchedUsers = users.filter(function(user){
+    return user.name.toLowerCase().indexOf(q)!==-1;
+})
+    res.render('users/create',{
+        users: matchedUsers
+    })
+})
+// thêm dữ liệu vào trong database
+
+ app.post('/users/create',  function(req, res){
+          users.push(req.body);
+          res.redirect('/users');
+ });
+
+
 app.listen(port, () => console.log(`Example app listen on port ${port}!`));
 
 
