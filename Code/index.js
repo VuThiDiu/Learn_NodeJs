0ead20e70c6b3b47780a45aa4b7c5a64d05f27dc
VuThiDiu <56347983+VuThiDiu@users.noreply.github.com>
2020-05-16 22:11:09 +0700

    add function search

--- Code/index.js
+++ Code/index.js
@@ -1,7 +1,12 @@
 const express= require('express')
 const app = express();
 const port = 3000;
-
+var  users=[
+    {id :1, name:'Vũ Trang Linh'},
+    {id :2, name:'Nguyễn Lê Bảo Thanh'},
+    {id :3, name:'Phạm Bạch Tuyết'},
+    {id :4, name:'Nguyễn Linh Nam'},
+]
 app.set('view engine','pug');
 app.set('views', './views');
 
@@ -10,14 +15,18 @@ app.get('/',(req, res) => res.render('index', {
 }));
 app.get('/users', function(req, res){
     res.render('users/index', {
-        users:[
-            {id :1, name:'Vũ Trang Linh'},
-            {id :2, name:'Nguyễn Lê Bảo Thanh'},
-            {id :3, name:'Phạm Bạch Tuyết'},
-            {id :4, name:'Nguyễn Linh Nam'},
-        ]
+        users: users
     })
 })
+app.get('/users/search', function (req, res){
+    var  q=  req.query.q;
+    var matchedUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q)!==-1;
+    })
+    res.render('users/index',{
+        users: matchedUsers
+    })
+});
 app.listen(port, () => console.log(`Example app listen on port ${port}!`));
 
 
