fe313d1fa27405478239d7d34695d2b101d91aa8
VuThiDiu <56347983+VuThiDiu@users.noreply.github.com>
2020-05-17 16:46:56 +0700

    create database(lowdb)

--- Code/index.js
+++ Code/index.js
@@ -6,13 +6,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-var  users=[
-    {id :1, name:'Vũ Trang Linh'},
-    {id :2, name:'Nguyễn Lê Bảo Thanh'},
-    {id :3, name:'Phạm Bạch Tuyết'},
-    {id :4, name:'Nguyễn Linh Nam'},
-]
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+ db= low(adapter);
+ // Set some defaults (required if your JSON file is empty)
+ db.defaults({ users: []})
+ .write()
+// var  users=[
+//     {id :1, name:'Vũ Trang Linh'},
+//     {id :2, name:'Nguyễn Lê Bảo Thanh'},
+//     {id :3, name:'Phạm Bạch Tuyết'},
+//     {id :4, name:'Nguyễn Linh Nam'},
+// ]
 
 
 app.set('view engine','pug');// chuyển css sang kiểu dữ liệu của pug
@@ -22,9 +28,11 @@ app.set('views', './views');
 app.get('/',(req, res) => res.render('index', {
     name:'hoamayman'
 }));
+
 app.get('/users', function(req, res){
     res.render('users/index', {
-        users: users
+        // in ra users tu file co san db.json
+        users: db.get('users').value()
     })
 })
 
@@ -32,7 +40,7 @@ app.get('/users', function(req, res){
 
 app.get('/users/search', function (req, res){
     var  q=  req.query.q;
-    var matchedUsers = users.filter(function(user){
+    var matchedUsers = db.get('users').value().filter(function(user){
         return user.name.toLowerCase().indexOf(q)!==-1;
     })
     res.render('users/index',{
@@ -42,18 +50,12 @@ app.get('/users/search', function (req, res){
 
 // function create  
 app.get('/users/create', function(req, res){
-    var  q=  req.query.q;
-var matchedUsers = users.filter(function(user){
-    return user.name.toLowerCase().indexOf(q)!==-1;
-})
-    res.render('users/create',{
-        users: matchedUsers
-    })
+    res.render('users/create')
 })
 // thêm dữ liệu vào trong database
-
+//.write(). lưu lại vào file cho mk
  app.post('/users/create',  function(req, res){
-          users.push(req.body);
+          db.get('users').push(req.body).write();
           res.redirect('/users');
  });
 
